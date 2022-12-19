117433fca59e2ef5d0cabf54d20eb2d2e83ac684
mersibon <mersibonmelese55@gmail.com>
2022-12-19 09:49:22 +0300

    binary2Uint8for_mask

--- preprocessing/binarymask2uint8mask.py
+++ preprocessing/binarymask2uint8mask.py
@@ -1,1 +1,14 @@
 
+import os
+import cv2
+import matplotlib.pyplot as plt
+from matplotlib import pyplot,image
+import glob
+
+path = glob.glob('F:/theises/proposal/Dataset/Organized Dataset/Ischemic Stroke/0538050/Mask/*.png')
+#print(img*255)
+for i in path:
+    print(i)
+    img=cv2.imread(i)
+    img=img*255
+    cv2.imwrite(i,img)
