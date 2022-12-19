a573127927e03bf963cd71e9015d36a79a643b4f
mersibon <mersibonmelese55@gmail.com>
2022-12-19 10:01:08 +0300

    'mask'

--- preprocessing/binarymask2uint8mask.py
+++ preprocessing/binarymask2uint8mask.py
@@ -1,4 +1,5 @@
 
+# import requered packages
 import os
 import cv2
 import matplotlib.pyplot as plt
@@ -6,9 +7,10 @@ from matplotlib import pyplot,image
 import glob
 
 path = glob.glob('F:/theises/proposal/Dataset/Organized Dataset/Ischemic Stroke/0538050/Mask/*.png')
-#print(img*255)
+# creating loop that iterate for each mask images in the folder
 for i in path:
     print(i)
     img=cv2.imread(i)
+    # multiplication of float values by 255 to obtain Uint8 pixel value of images
     img=img*255
     cv2.imwrite(i,img)
